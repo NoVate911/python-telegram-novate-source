295a2d74a51e3aa637c52cbe9cec6de428a3bf6d
novate911 <ritcot-delovoy@yandex.ru>
2023-11-12 01:48:15 +0300

    The distribution of messages to the Administration was made when a new request was received

--- app/handlers/user/request.py
+++ app/handlers/user/request.py
@@ -1,8 +1,11 @@
+import logging
+
 from aiogram import Router, F
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import StateFilter, Command
 
+from app.database.requests.select import user_all_telegram_id as select_user_all_telegram_id, user_administrator_by_telegram_id as select_user_administrator_by_telegram_id
 from app.misc.filters import IsSubscribedToChannels, IsRegistered, NotAdministrator
 from app.misc.keyboards import main as main_kb, request as request_kb
 from app.misc.states import RequestStates
@@ -42,6 +45,13 @@ async def cmd_request_create(msg: Message, state: FSMContext) -> None:
         return
     await state.set_state(state=RequestStates.MAIN)
     await msg.reply(text=translations[user_language]['messages']['user']['request']['create']['success'], reply_markup=await request_kb(msg=msg))
+    users_telegram_id: list[tuple[int]] = await select_user_all_telegram_id()
+    for user_telegram_id in users_telegram_id:
+        if await select_user_administrator_by_telegram_id(telegram_id=user_telegram_id):
+            try:
+                await msg.bot.send_message(chat_id=user_telegram_id, text=str.format(translations[user_language]['messages']['user']['request']['create']['notification_administrator'], msg.from_user.username, msg.from_user.id, msg.text))
+            except Exception as ex:
+                logging.error(ex)
 
 for language in languages:
     @router.message(StateFilter(RequestStates.MAIN), IsSubscribedToChannels(), IsRegistered(), NotAdministrator(), F.text.lower() == translations[language]['keyboards']['reply']['user']['request']['back'].lower())
